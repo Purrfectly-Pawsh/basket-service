0a0298e565f8afcc31f3dc435c2f3d23bc49af26
Lor0l <o.lordieck@web.de>
2024-05-24 14:38:37 +0200

    initial commit

---
+++ src/main/java/de/htw/basketmicroservice/BasketMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package de.htw.basketmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/htw/basketmicroservice/BasketMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.basketmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
