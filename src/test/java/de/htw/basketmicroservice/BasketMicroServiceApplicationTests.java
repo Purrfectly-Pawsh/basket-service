391192ccd051fea6a156f8463288f4cea3cf0088
Kristiyan <74624512+krisg9@users.noreply.github.com>
2024-05-30 14:22:45 +0200

    Update BasketMicroServiceApplicationTests.java

--- src/test/java/de/htw/basketmicroservice/BasketMicroServiceApplicationTests.java
+++ src/test/java/de/htw/basketmicroservice/BasketMicroServiceApplicationTests.java
@@ -3,11 +3,6 @@ package de.htw.basketmicroservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BasketMicroServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
