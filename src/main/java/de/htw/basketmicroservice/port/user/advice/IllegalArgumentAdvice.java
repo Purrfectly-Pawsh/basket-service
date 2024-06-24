1d084c55a045052ae3173e4a934637e20582f549
Lor0l <o.lordieck@web.de>
2024-06-24 16:01:30 +0200

    added advice

---
+++ src/main/java/de/htw/basketmicroservice/port/user/advice/IllegalArgumentAdvice.java
@@ -0,0 +1,27 @@
+package de.htw.basketmicroservice.port.user.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class IllegalArgumentAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String illegalArgumentException(IllegalArgumentException e) {
+        log.atError().log(e.getMessage());
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String nullPointerException(NullPointerException e) {
+        log.atError().log(e.getMessage());
+        return e.getMessage();
+    }
+
+}
