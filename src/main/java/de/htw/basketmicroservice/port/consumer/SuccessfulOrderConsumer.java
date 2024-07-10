061943f2c632ed5b6c7172bd5731f3bbbe92cf49
Lor0l <o.lordieck@web.de>
2024-07-10 11:57:58 +0200

    log removed

--- src/main/java/de/htw/basketmicroservice/port/consumer/SuccessfulOrderConsumer.java
+++ src/main/java/de/htw/basketmicroservice/port/consumer/SuccessfulOrderConsumer.java
@@ -1,22 +1,16 @@
 package de.htw.basketmicroservice.port.consumer;
 
 import de.htw.basketmicroservice.config.RabbitMQConfig;
-import de.htw.basketmicroservice.core.domain.model.BasketItemKey;
 import de.htw.basketmicroservice.core.domain.service.inferfaces.IBasketService;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.UUID;
 
 
 @Component
 public class SuccessfulOrderConsumer {
 
-    private static final Logger log = LoggerFactory.getLogger(SuccessfulOrderConsumer.class);
     private final IBasketService basketService;
 
     public SuccessfulOrderConsumer(IBasketService basketService) {
