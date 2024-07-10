1255a9eceeab24817abeace5c85fb73e70195215
Lor0l <o.lordieck@web.de>
2024-07-10 11:50:14 +0200

    SuccessfullOrderMessage with userId only

--- src/main/java/de/htw/basketmicroservice/core/domain/service/inferfaces/IBasketService.java
+++ src/main/java/de/htw/basketmicroservice/core/domain/service/inferfaces/IBasketService.java
@@ -16,4 +16,6 @@ public interface IBasketService {
    void changeBasketItemQuantity(UUID basketId, UUID itemId, int quantity);
 
    void transferGuestToUserBasket(UUID guestBasketId, UUID userId);
+
+   void removeBasket(UUID basketId);
 }
--- src/main/java/de/htw/basketmicroservice/port/consumer/SuccessfulOrderConsumer.java
+++ src/main/java/de/htw/basketmicroservice/port/consumer/SuccessfulOrderConsumer.java
@@ -1,15 +1,22 @@
 package de.htw.basketmicroservice.port.consumer;
 
 import de.htw.basketmicroservice.config.RabbitMQConfig;
+import de.htw.basketmicroservice.core.domain.model.BasketItemKey;
 import de.htw.basketmicroservice.core.domain.service.inferfaces.IBasketService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
+
 
 @Component
 public class SuccessfulOrderConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(SuccessfulOrderConsumer.class);
     private final IBasketService basketService;
 
     public SuccessfulOrderConsumer(IBasketService basketService) {
@@ -18,10 +25,7 @@ public class SuccessfulOrderConsumer {
 
     @RabbitListener(queues = RabbitMQConfig.CREATE_ORDER_QUEUE)
     public void receiveCreateOrderEvent(SuccessfulOrderMessage message) {
-        List<BasketItemKey> basketItemKeys = SuccessfulOrderMapper.toBasketItemKeys(message);
-        for (BasketItemKey basketItemKey : basketItemKeys) {
-            basketService.removeBasketItem(basketItemKey.getBasketId(), basketItemKey.getBasketItemId());
-        }
+        basketService.removeBasket(UUID.fromString(message.getUserId()));
     }
 
 }
